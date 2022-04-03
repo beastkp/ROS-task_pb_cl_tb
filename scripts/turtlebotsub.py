ef31a678ce0d8af7b1d5b15a06211c32ae1e2e7d
KrishP <91586330+beastkp@users.noreply.github.com>
2022-04-03 17:08:23 +0530

    Update turtlebotsub.py
    still trying

--- scripts/turtlebotsub.py
+++ scripts/turtlebotsub.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
 import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String 
 from assign1.srv import *
 
 radius = 5
 def callback1():
-    radius = int(data.data)
     while not rospy.is_shutdown():
-        computeangvel = rospy.ServiceProxy('compute_ang_vel',compute_ang_vel)
-        rospy.Publisher("cmd_vel",float ,queue_size=10)
+        try:
+            computeangvel = rospy.ServiceProxy('compute_ang_vel',compute_ang_vel)
+            resp1 = computeangvel(int(radius))
+            return resp1.angvel
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
+pub = rospy.Publisher("cmd_vel",Twist,queue_size=1)
 
 
 def turtlebotsub():
     rospy.init_node('turtlebotsub')
-    rospy.Subscriber("\radius",int, callback1)
+    rospy.Subscriber("\radius",String, callback1)
+    rospy.spin()
     rospy.wait_for_service('compute_ang_vel')
         
 if __name__ == '__main__':
